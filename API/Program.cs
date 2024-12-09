6d3a6106ebd7169a25120ecc4fea8c6e5c2b5948
neddydale02 <vittoria.marella@gmail.com>
2024-12-09 12:30:43 +0100

    End of Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,18 +8,14 @@ builder.Services.AddDbContext<API.Data.DataContext>(opt =>
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+// Ensure CORS is applied before other middleware
+app.UseCors(x => x.AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .WithOrigins("http://localhost:4200", "https://localhost:4200"));
 
 app.UseHttpsRedirection();
 
